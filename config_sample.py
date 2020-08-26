b7c97136fdeb5bb565463bd50f18b3244d79d211
Singularity <justin.parker.12@gmail.com>
2020-08-26 17:06:22 -0500

    config_sample

---
+++ config_sample.py
@@ -0,0 +1,2 @@
+login_details = {'username': 'myemail+ta@email.com',
+                 'password': 'mYp@ssw0rd'}
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from scrape import approve
-from config import login_details
 from pathlib import Path
 
 if __name__ == "__main__":
-    approve(login_details['username'],
-            login_details['password'], headless=False, append_grader=True)
+    try:
+        from config import login_details
+        approve(login_details['username'],
+                login_details['password'], headless=False, append_grader=True)
+    except:
+        approve(append_grader=True)
